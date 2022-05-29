30ba6e93eacac83f7603813c45e7f8b1aef7dc55
calle <Callerin@live.com>
2022-05-29 15:03:44 +0800

    按钮放在最前面

--- embyLaunchPot.js
+++ embyLaunchPot.js
@@ -1,4 +1,3 @@
-
 // ==UserScript==
 // @name embyLaunchPotplayer
 // @namespace http://tampermonkey.net/
@@ -9,14 +8,14 @@
 // @grant none
 // @require https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
 // ==/UserScript==
-var timer = setInterval(function () {
-	var potplayer = $("button[is='embyPot']:not(.hide) ")[0];
+var timer = setInterval(function() {
+    var potplayer = $("button[is='embyPot']:not(.hide) ")[0];
 
-	if (!potplayer) {
-		var mainDetailButtons = $("div[is='emby-scroller']:not(.hide) .mainDetailButtons")[0];
-		if (mainDetailButtons) {
-			var html = mainDetailButtons.innerHTML;
-			let buttonhtml = `<button is="embyPot" type="button"
+    if (!potplayer) {
+        var mainDetailButtons = $("div[is='emby-scroller']:not(.hide) .mainDetailButtons")[0];
+        if (mainDetailButtons) {
+            var html = mainDetailButtons.innerHTML;
+            let buttonhtml = `<button is="embyPot" type="button"
 	class="btnPlay btnMainPlay raised detailButton emby-button emby-button-backdropfilter raised-backdropfilter detailButton-primary"
 	data-mode="play" title="Potplayer" aria-label="Potplayer">
 	<i class="md-icon button-icon button-icon-left"></i>
@@ -24,15 +23,15 @@ var timer = setInterval(function () {
 </button>`
 
 
-			mainDetailButtons.insertAdjacentHTML('beforeEnd', buttonhtml) // 按钮插入位置
-			document.querySelector("button[is='embyPot']:not(.hide)").onclick = embyPot;
-		}
+            mainDetailButtons.insertAdjacentHTML('afterBegin', buttonhtml) // 按钮插入位置
+            document.querySelector("button[is='embyPot']:not(.hide)").onclick = embyPot;
+        }
 
-	}
+    }
 }, 400)
 
 async function embyPot() {
-	var mediaUrl = $("div[is='emby-scroller']:not(.hide) div.sectionTitle.sectionTitle-cards div").siblings('div')[0];
-	var url = "emby://" + mediaUrl.innerHTML;
-	window.location.href = url;
+    var mediaUrl = $("div[is='emby-scroller']:not(.hide) div.sectionTitle.sectionTitle-cards div").siblings('div')[0];
+    var url = "emby://" + mediaUrl.innerHTML;
+    window.location.href = url;
 }
